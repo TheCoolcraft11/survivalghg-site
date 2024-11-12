44666333d977638ecbafe282fa4ed594556927ec
“TheCoolcraft11” <max.apps+github@outlook.de>
2024-11-12 19:07:54 +0000

    Added About.html Changed dashboard.html Improved performance

--- public/widgets/js/process.js
+++ public/widgets/js/process.js
@@ -3,22 +3,20 @@ function fetchTopProcesses() {
         .then(response => response.json())
         .then(processes => {
             const processList = document.getElementById("process-list");
-            processList.innerHTML = ''; // Vorherige Einträge leeren
+            processList.innerHTML = '';
 
             processes.forEach(process => {
                 const listItem = document.createElement("li");
+                listItem.classList.add('process-list');
 
-                // Erstelle eine gekürzte Version des Befehls, falls er zu lang ist
                 const truncatedCommand = process.command.length > 30 ? process.command.substring(0, 30) + '...' : process.command;
 
                 listItem.textContent = `PID: ${process.pid} | User: ${process.user} | CPU: ${process.cpu}% | RAM: ${process.mem}% | Command: ${truncatedCommand}`;
 
-                // Erstelle ein Element für den Tooltip mit dem vollständigen Befehl
                 const tooltip = document.createElement("span");
                 tooltip.className = "tooltip";
                 tooltip.textContent = process.command;
 
-                // Füge den Tooltip zum Listenelement hinzu
                 listItem.appendChild(tooltip);
 
                 processList.appendChild(listItem);
